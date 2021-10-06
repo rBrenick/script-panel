2279b4603abfcd8b9002057599bbc7a7b833fcb6
Richard Brenick <richard.brenick@hotmail.com>
2021-10-06 21:46:46 +0200

    fixed up reload_modules

--- script_panel/__init__.py
+++ script_panel/__init__.py
@@ -3,11 +3,15 @@ def main():
     from script_panel import script_panel_ui
     return script_panel_ui.main()
 
+
 def reload_modules():
+    import sys
+    if sys.version_info.major >= 3:
+        from importlib import reload
+    else:
+        from imp import reload
+
     from script_panel import script_panel_utils
     from script_panel import script_panel_ui
     reload(script_panel_utils)
     reload(script_panel_ui)
-
-
-
